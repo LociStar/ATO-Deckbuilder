1c3fc5bc29c22951b4932efb7f1c87dfa9dfcf9b
LociStar <locistar.got@gmail.com>
2024-03-05 14:49:11 +0100

    extracted Search as CustomSearch component

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,15 +1,12 @@
-import {styled, alpha} from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
 import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
@@ -19,46 +16,9 @@ import {JSX} from 'preact';
 import {useAuth} from "react-oidc-context";
 import RenderOnAuthenticated from "./conditionals/RenderOnAuthenticated.tsx";
 import RenderOnAnonymous from "./conditionals/RenderOnAnonymous.tsx";
+import CustomSearch from "./CustomSearch.tsx";
 
-const Search = styled('div')(({theme}) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-    },
-}));
 
-const SearchIconWrapper = styled('div')(({theme}) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({theme}) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
-    },
-}));
 
 export default function PrimarySearchAppBar({setSearchQuery}: { setSearchQuery: (query: string) => void }) {
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -186,17 +146,18 @@ export default function PrimarySearchAppBar({setSearchQuery}: { setSearchQuery: 
                 >
                     ATO-Deckbuilder
                 </Typography>
-                <Search>
-                    <SearchIconWrapper>
-                        <SearchIcon/>
-                    </SearchIconWrapper>
-                    <StyledInputBase
-                        placeholder="Search…"
-                        inputProps={{'aria-label': 'search'}}
-                        onChange={(event: Event) => setSearchQuery((event.target as HTMLInputElement).value)} // Update the search query state when the user types in the search bar
+                <CustomSearch setSearchQuery={setSearchQuery}/>
+                {/*<Search>*/}
+                {/*    <SearchIconWrapper>*/}
+                {/*        <SearchIcon/>*/}
+                {/*    </SearchIconWrapper>*/}
+                {/*    <StyledInputBase*/}
+                {/*        placeholder="Search…"*/}
+                {/*        inputProps={{'aria-label': 'search'}}*/}
+                {/*        onChange={(event: Event) => setSearchQuery((event.target as HTMLInputElement).value)} // Update the search query state when the user types in the search bar*/}
 
-                    />
-                </Search>
+                {/*    />*/}
+                {/*</Search>*/}
                 <Box sx={{flexGrow: 1}}/>
                 <Box sx={{display: {xs: 'none', md: 'flex'}}}>
                     <IconButton size="large" aria-label="show 4 new mails" color="inherit">
