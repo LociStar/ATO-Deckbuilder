11dde256438dd110ce72871777948bda9c6f8c7e
LociStar <locistar.got@gmail.com>
2024-03-06 18:53:08 +0100

    added auth to api calls and refactored code

--- src/screens/CardsView.tsx
+++ src/screens/CardsView.tsx
@@ -1,6 +1,8 @@
 import {useState, useEffect} from 'preact/hooks';
 import {Button, Stack} from "@mui/material";
 import {Card} from "../types/types.tsx";
+import {useAuth} from "react-oidc-context";
+import CardComponent from "../components/CardComponent.tsx";
 
 export default function CardsView({searchQuery, charClass, secondaryCharClass, component, onCardClick}: {
     searchQuery: string,
@@ -11,34 +13,43 @@ export default function CardsView({searchQuery, charClass, secondaryCharClass, c
 }) {
     const [cards, setCards] = useState<Card[]>([]);
     const [page, setPage] = useState(0);
+    const auth = useAuth();
 
     // Reset page to 0 when searchQuery changes
     useEffect(() => {
         setPage(0);
     }, [searchQuery]);
 
-    useEffect(() => {
-        fetch(`http://localhost:8080/card?page=${page}&size=24&searchQuery=${searchQuery}&charClass=${charClass}&secondaryCharClass=${secondaryCharClass}`, {method: 'GET'})
-            .then(response => response.json())
+    const fetchCards = async ({page, searchQuery, charClass, secondaryCharClass, token}: { page: any, searchQuery: any, charClass: any, secondaryCharClass: any, token: any }) => {
+        fetch(`http://localhost:8080/card?page=${page}&size=24&searchQuery=${searchQuery}&charClass=${charClass}&secondaryCharClass=${secondaryCharClass}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + token,
+                }
+            }).then(response => response.json())
             .then(data => setCards(data));
-    }, [page, searchQuery]);
+    }
+
+    useEffect(() => {
+        if (!auth.user?.access_token) return;
+        try {
+            fetchCards({page: page, searchQuery: searchQuery, charClass: charClass, secondaryCharClass: secondaryCharClass, token: auth.user?.access_token}).then(r => r);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }, [page, searchQuery, charClass, secondaryCharClass, auth.user?.access_token]);
 
     return (
         <Stack direction="column">
             <div style={{display: 'grid', gridTemplateColumns: 'repeat(6, 1fr)', gridGap: '10px'}}>
                 {cards.map((card) => (
-                    <div style={{width: '100%', paddingBottom: '151.52%', position: 'relative'}}>
-                        <img className="zoomEffect"
-                             src={`http://localhost:8080/image/${card.id}`}
-                             alt={`Card ${card.id}`}
-                             style={{position: 'absolute', width: '100%', height: '100%',}}
-                             onClick={() => onCardClick(card)}
-                        />
-                    </div>
+                    <CardComponent card={card} token={auth.user?.access_token} onCardClick={onCardClick}/>
                 ))}
             </div>
             {component ? (
-                <Stack spacing={{xs: 1, sm: 2}} direction="row" justifyContent={"center"} useFlexGap>
+                <Stack spacing={{xs: 1, sm: 2}} sx={{marginTop: 2}} direction="row" justifyContent={"center"}
+                       useFlexGap>
                     <Button variant="contained" onClick={() => setPage(oldPage => Math.max(oldPage - 1, 0))}>Previous
                         page</Button>
                     <Button variant="contained" onClick={() => setPage(oldPage => oldPage + 1)}>Next page</Button>
