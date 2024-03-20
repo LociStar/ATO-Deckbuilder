78b6056c3b9be7953c9932dbd190516dfc9e3770
LociStar <locistar.got@gmail.com>
2024-03-20 17:10:43 +0100

    init BarChart

---
+++ src/components/graphs/RarityGraph.tsx
@@ -0,0 +1,82 @@
+import { BarChart } from '@mui/x-charts/BarChart';
+import {Card} from "../../types/types.tsx";
+import {useEffect, useState} from "preact/hooks";
+
+type RarityGraphProps = {
+    cardList: Card[];
+};
+
+const rarityColors = {
+    'Common': '#6d6d6d',
+    'Uncommon': '#1f77b4',
+    'Rare': '#2ca02c',
+    'Epic': '#d62728',
+    'Mythic': '#9467bd'
+};
+
+export const RarityGraph: React.FC<RarityGraphProps> = ({ cardList }) => {
+    const [rarityDataset, setRarityDataset] =
+        useState<{'Common': number, 'Uncommon': number, 'Rare': number, 'Epic': number, 'Mythic': number}>({
+            'Common': 0,
+            'Uncommon': 0,
+            'Rare': 0,
+            'Epic': 0,
+            'Mythic': 0
+        });
+
+    useEffect(() => {
+        const rarities = cardList.reduce((acc: { [key: string]: number }, card: Card) => {
+            acc[card.rarity] = (acc[card.rarity] || 0) + 1;
+            return acc;
+        }, {});
+
+        // set card rarity to 0 if it doesn't exist
+        ['Common', 'Uncommon', 'Rare', 'Epic', 'Mythic'].forEach((rarity) => {
+            if (!rarities[rarity]) {
+                rarities[rarity] = 0;
+            }
+        });
+
+        setRarityDataset(rarities);
+    }, [cardList]);
+    return (
+        <BarChart width={500} height={300}
+                  slotProps={{
+                      legend: {
+                          direction: 'row',
+                          position: {vertical: 'bottom', horizontal: 'middle'},
+                          padding: 0,
+                      },
+                  }}
+                  series={[
+                      {
+                          label: 'Common',
+                          data: [rarityDataset['Common']],
+                          color: rarityColors['Common'],
+                      },
+                      {
+                          label: 'Uncommon',
+                          data: [rarityDataset['Uncommon']],
+                          color: rarityColors['Uncommon'],
+                      },
+                      {
+                          label: 'Rare',
+                          data: [rarityDataset['Rare']],
+                          color: rarityColors['Rare'],
+                      },
+                      {
+                          label: 'Epic',
+                          data: [rarityDataset['Epic']],
+                          color: rarityColors['Epic'],
+                      },
+                      {
+                          label: 'Mythic',
+                          data: [rarityDataset['Mythic']],
+                          color: rarityColors['Mythic'],
+                      },
+                  ]}
+                  xAxis={[{scaleType: 'band', data: ['Rarity']}]}
+                  yAxis={[{scaleType: 'linear', label: 'Amount'}]}
+        />
+    );
+};
---
+++ src/screens/ViewController.tsx
@@ -0,0 +1,9 @@
+import DeckBuilder from "./DeckBuilder.tsx";
+import DeckDetailsView from "./DeckDetailsView.tsx";
+
+export default function ViewController() {
+    return (
+        // <DeckBuilder/>
+        <DeckDetailsView/>
+    );
+}
