59c1941c27dd095eb5473009947a2dbbc7d44762
LociStar <locistar.got@gmail.com>
2024-03-22 19:01:05 +0100

    renamed component

--- src/screens/DecksView.tsx
+++ src/screens/DecksView.tsx
@@ -1,24 +1,24 @@
 import {Fab, Stack} from "@mui/material";
 import CharCard from "../components/CharCard";
 import {useState, useEffect} from "preact/hooks";
-import {CharBuild} from "../types/types";
+import {Deck} from "../types/types";
 import AddIcon from '@mui/icons-material/Add';
 import Box from "@mui/material/Box";
 
-export default function DeckBuilds() {
-    const [charBuilds, setCharBuilds] = useState<CharBuild[]>([]);
+export default function DecksView() {
+    const [decks, setDecks] = useState<Deck[]>([]);
 
     useEffect(() => {
         fetch('http://localhost:8080/deck')
             .then(response => response.json())
-            .then(data => setCharBuilds(data));
+            .then(data => setDecks(data));
     }, []);
 
     return (
         <Box sx={{ '& > :not(style)': { m: 1 } }}>
             <Stack alignItems="center" justifyContent={'center'} spacing={2}>
-                {charBuilds.map((charBuild) => (
-                    <CharCard key={charBuild.id} charBuild={charBuild}/>
+                {decks.map((deck) => (
+                    <CharCard key={deck.id} deck={deck}/>
                 ))}
             </Stack>
             <Fab color="secondary" aria-label="add" style={{position: 'fixed', right: '10px', bottom: '10px'}}>
