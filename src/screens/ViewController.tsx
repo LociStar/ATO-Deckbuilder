2cfebac7cb725e2a90eeb1bf4a2bb3bf18446dc0
LociStar <locistar.got@gmail.com>
2024-03-22 19:01:33 +0100

    init ViewController

--- src/screens/ViewController.tsx
+++ src/screens/ViewController.tsx
@@ -1,9 +1,28 @@
 import DeckBuilder from "./DeckBuilder.tsx";
 import DeckDetailsView from "./DeckDetailsView.tsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import DecksView from "./DecksView.tsx";
 
 export default function ViewController() {
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <DecksView/>,
+        },
+        {
+            path: "/deck/*",
+            element: <DeckDetailsView/>,
+        },
+        {
+            path: "/deckbuilder",
+            element: <DeckBuilder/>,
+        },
+    ]);
+
     return (
-        // <DeckBuilder/>
-        <DeckDetailsView/>
+        <div>
+            <RouterProvider router={router}/>
+        </div>
     );
 }
