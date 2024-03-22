fbfe7d623395e940d71ff552f172f84ecf266de0
LociStar <locistar.got@gmail.com>
2024-03-22 12:30:37 +0100

    changed theme

--- src/app.tsx
+++ src/app.tsx
@@ -4,15 +4,21 @@ import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import AppBar from "./components/AppBar.tsx";
 import CardsView from "./screens/CardsView.tsx";
 import {useState} from "preact/hooks";
-import CharBuilds from "./screens/CharBuilds.tsx";
+import DeckBuilds from "./screens/DeckBuilds.tsx";
 import {oidcConfig} from "./config.tsx";
 import {AuthProvider} from "react-oidc-context";
+import DeckBuilder from "./screens/DeckBuilder.tsx";
+import ViewController from "./screens/ViewController.tsx";
+import CantoraOne from "./assets/fonts/font.ttf";
 
 declare module '@mui/material/styles' {
     interface Theme {
         status: {
             danger: string;
         };
+        topography: {
+            fontFamily: string;
+        }
     }
 
     // allow configuration using `createTheme`
@@ -20,25 +26,50 @@ declare module '@mui/material/styles' {
         status?: {
             danger?: string;
         };
+        topography?: {
+            fontFamily?: string;
+        }
     }
 }
 
 const theme = createTheme({
+    // palette: {
+    //     background: {
+    //         default: 'gray',
+    //     },
+    //     primary: {
+    //         light: '#616161',
+    //         main: '#424242',
+    //         dark: '#212121',
+    //         contrastText: '#ffffff',
+    //     },
+    //     secondary: {
+    //         light: '#eeeeee',
+    //         main: '#bdbdbd',
+    //         dark: '#616161',
+    //         contrastText: '#000',
+    //     },
+    // },
     palette: {
-        background: {
-            default: 'gray',
-        },
-        primary: {
-            light: '#616161',
-            main: '#424242',
-            dark: '#212121',
-            contrastText: '#ffffff',
-        },
-        secondary: {
-            light: '#eeeeee',
-            main: '#bdbdbd',
-            dark: '#616161',
-            contrastText: '#000',
+        mode: 'dark', primary: {
+            main: '#414FD8',
+        }
+    },
+    typography: {
+        fontFamily: 'CantoraOne',
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: `
+        @font-face {
+          font-family: 'CantoraOne';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          src: local('CantoraOne'), local('CantoraOne-Regular'), url(${CantoraOne}) format('ttf');
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
         },
     },
 });
@@ -53,7 +84,8 @@ export function App() {
                 <CssBaseline/>
                 <AppBar setSearchQuery={setSearchQuery}/>
                 {/*<CardsView searchQuery={searchQuery}/>*/}
-                <CharBuilds/>
+                {/*<DeckBuilds/>*/}
+                <ViewController/>
             </ThemeProvider>
             </AuthProvider>
         </div>
