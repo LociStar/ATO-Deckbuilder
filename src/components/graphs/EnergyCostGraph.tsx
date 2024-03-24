6a3d1d87782661ae2f933f67927e421a0cf5d504
LociStar <locistar.got@gmail.com>
2024-03-24 12:15:31 +0100

    changed style

--- src/components/graphs/EnergyCostGraph.tsx
+++ src/components/graphs/EnergyCostGraph.tsx
@@ -8,23 +8,27 @@ type EnergyCostGraphProps = {
 };
 
 export const EnergyCostGraph: React.FC<EnergyCostGraphProps> = ({cardList}) => {
-    const [energyCostDataset, setEnergyCostDataset] = useState<{ energy: string, amount: number }[]>([{
-        energy: '1',
-        amount: 0
-    }, {energy: '2', amount: 0}, {energy: '3', amount: 0}, {energy: '4', amount: 0}, {energy: '5+', amount: 0}]);
+    const [energyCostDataset, setEnergyCostDataset] = useState<{ energy: string, amount: number }[]>([
+        {energy: '0', amount: 0}, {energy: '1', amount: 0}, {energy: '2', amount: 0}, {
+            energy: '3',
+            amount: 0
+        }, {energy: '4', amount: 0}, {energy: '5+', amount: 0}]);
     const [averageEnergyCost, setAverageEnergyCost] = useState<number>(0);
 
     useEffect(() => {
         let totalEnergyCost = 0;
+        console.log(cardList)
         const costs = cardList.reduce((acc: number[], card: Card) => {
-            if (card.energyCost >= 1 && card.energyCost <= 5) {
+            if (card.energyCost == 0)
+                acc[0]++;
+            else if (card.energyCost >= 1 && card.energyCost <= 4) {
                 acc[card.energyCost - 1]++;
-            } else if (card.energyCost > 5) {
-                acc[4]++;
+            } else if (card.energyCost >= 5) {
+                acc[5]++;
             }
             totalEnergyCost += card.energyCost;
             return acc;
-        }, [0, 0, 0, 0, 0]);
+        }, [0, 0, 0, 0, 0, 0]);
 
         const averageCost = totalEnergyCost / cardList.length;
 
@@ -34,7 +38,7 @@ export const EnergyCostGraph: React.FC<EnergyCostGraphProps> = ({cardList}) => {
         }));
 
         setEnergyCostDataset(newEnergyCostDataset);
-        setAverageEnergyCost(averageCost);
+        setAverageEnergyCost(Math.round(averageCost * 100) / 100);
     }, [cardList]);
     return (
         <>
@@ -50,7 +54,7 @@ export const EnergyCostGraph: React.FC<EnergyCostGraphProps> = ({cardList}) => {
                               color: '#cc8b00',
                           },
                       ]}
-                      xAxis={[{scaleType: 'band', dataKey: 'energy', label: 'Energy'}]}
+                      xAxis={[{scaleType: 'band', data: [0, 1, 2, 3, 4, '5+'], label: 'Energy'}]}
             />
         </>
     );
