ef71afcb1d6f0a2b99fba5bbac77f017c06fb909
LociStar <locistar.got@gmail.com>
2024-03-24 13:00:02 +0100

    removed unused types

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -14,28 +14,14 @@ export type Deck = {
     id: number;
     title: string;
     description: string;
-    charName: string;
     username: string;
     likes: number;
     characterId: string;
     cardList: Card[];
 };
 
-export type CharBuild = {
-    id: string;
-    title: string;
-    charName: string;
-    userName: string;
-    likes: number;
-};
-
 export type Character = {
     characterId: string;
     characterClass: string;
     secondaryCharacterClass: string;
-}
-
-export type CharacterItem = {
-    value: string;
-    label: string;
 }
