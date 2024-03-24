b0b09b91216bea927a9319820ec6878fee48db42
LociStar <locistar.got@gmail.com>
2024-03-24 12:14:35 +0100

    changed style, fixed list

--- src/screens/DeckBuilder.tsx
+++ src/screens/DeckBuilder.tsx
@@ -1,14 +1,31 @@
-import {Box, Button, Divider, List, ListItem, ListItemText, Stack, TextField} from "@mui/material";
+import {
+    Alert,
+    Box,
+    Button,
+    Divider,
+    FormControl,
+    InputLabel,
+    ListItem, ListItemButton,
+    ListItemText, Select, Snackbar,
+    Stack, Tab, Tabs,
+    TextField
+} from "@mui/material";
 import Grid from '@mui/material/Unstable_Grid2';
 import MenuItem from "@mui/material/MenuItem";
 import CardsView from "./CardsView.tsx";
 import {useEffect, useState} from "preact/hooks";
 import {Card, Character} from "../types/types.tsx";
-import {memo} from 'preact/compat';
 import Typography from "@mui/material/Typography";
 import CustomSearch from "../components/CustomSearch.tsx";
 import {useAuth} from "react-oidc-context";
 import {calculate_deck_cost} from "../utils/utils.ts";
+import {alpha} from "@mui/material/styles";
+import {EnergyCostGraph} from "../components/graphs/EnergyCostGraph.tsx";
+import {RarityGraph} from "../components/graphs/RarityGraph.tsx";
+import {FixedSizeList} from 'react-window';
+import {AppConfig} from "../config.tsx";
+import {MuiMarkdown} from "mui-markdown";
+import {useNavigate} from "react-router-dom";
 
 export default function DeckBuilder() {
     const [title, setTitle] = useState('');
@@ -20,7 +37,11 @@ export default function DeckBuilder() {
     const [cardCost, setCardCost] = useState(0);
     const [cardCraftingModifier, setCardCraftingModifier] = useState(1);
     const [cardUpgradingModifier, setCardUpgradingModifier] = useState(1);
+    const [tabValue, setTabValue] = useState(0);
+    const [open, setOpen] = useState(false);
+    const [openError, setOpenError] = useState(false);
     const auth = useAuth();
+    const navigate = useNavigate();
 
     const removeCard = (cardToRemove: Card) => {
         setCardList(oldList => {
@@ -37,29 +58,31 @@ export default function DeckBuilder() {
     }, [cardList, cardCraftingModifier, cardUpgradingModifier]);
 
     useEffect(() => {
-        if (auth.user?.access_token) {
-            fetch('http://localhost:8080/character', {
-                method: 'GET',
-            })
-                .then(response => response.json())
-                .then(data => setChars(data));
-        }
-    }, [auth.user?.access_token]);
+        fetch(AppConfig.API_URL + '/character', {
+            method: 'GET',
+        })
+            .then(response => response.json())
+            .then(data => setChars(data));
+    }, []);
+
+    const handleTabChange = (_ignored: Event, newValue: number) => {
+        setTabValue(newValue);
+    };
 
     const addCardToList = (card: Card) => {
         setCardList(oldList => [...oldList, card]);
     };
 
-    const CustomListItem = ({card, removeCard}: { card: Card, removeCard: (card: Card) => void }) => (
-        <div>
-            <ListItem>
-                <Button size="small" variant="outlined" sx={{marginRight: 2}}
-                        onClick={() => removeCard(card)}>X</Button>
-                <ListItemText primary={card.name}/>
-            </ListItem>
-            <Divider/>
-        </div>
-    );
+    const handleClose = (_ignored: Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+        if (openError)
+            setIsSaving(false);
+        setOpenError(false);
+    };
 
     async function uploadDeck(title: string, description: string, cardList: Card[]) {
         setIsSaving(true);
@@ -69,96 +92,164 @@ export default function DeckBuilder() {
             cardList,
             characterId: selectedCharacter // Include the selected character in the deck object
         };
-        console.log(JSON.stringify(deck))
 
         try {
-            const response = await fetch('http://localhost:8080/deck/upload', {
+            await fetch('http://localhost:8080/deck/upload', {
                 method: 'PUT',
                 headers: {
                     'Authorization': 'Bearer ' + auth.user?.access_token,
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(deck)
+            }).then(response => {
+                return response.json();
+            }).then(data => {
+                setOpen(true);
+                navigate('/deck/' + data);
             });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-
-            console.log('Deck uploaded successfully');
         } catch (error) {
-            console.error('Error:', error);
+            setOpenError(true);
         }
     }
 
     return (
-        <Stack flex="auto" marginLeft={10} marginRight={10} marginBottom={2} spacing={2}>
-            <TextField
-                id="standard-basic"
-                label="Title"
-                variant="outlined"
-                value={title}
-                onChange={(e) => {
-                    const target = e.target as HTMLInputElement;
-                    setTitle(target?.value || '');
-                }}
-            />
-            <TextField
-                label="Description"
-                placeholder="test"
-                multiline
-                rows={5}
-                value={description}
-                onChange={(e) => {
-                    const target = e.target as HTMLInputElement;
-                    setDescription(target?.value || '');
-                }}
-            />
-            <TextField
-                id="character-field"
-                label="Character"
-                variant="outlined"
-                select
-                value={selectedCharacter} // Set the value to the selected character
-                onChange={(e) => {
-                    const target = e.target as HTMLInputElement;
-                    const selectedChar = chars.find(char => char.characterId === target.value);
-                    setSelectedCharacter(selectedChar ? selectedChar.characterId : '');
-                }}
+        <div>
+            <Stack flex="auto" marginLeft={10} marginRight={10} marginBottom={2} spacing={2} padding={2}
+                   sx={{
+                       backdropFilter: 'blur(50px)',
+                       backgroundColor: alpha('#000000', 0.5),
+                       borderRadius: 2,
+                       fontStyle: {color: 'white'}
+                   }}>
+                <TextField
+                    sx={{fontStyle: {color: 'white'}}}
+                    color="warning"
+                    label="Title"
+                    value={title}
+                    onChange={(e) => {
+                        const target = e.target as HTMLInputElement;
+                        setTitle(target?.value || '');
+                    }}
+                />
+                <Tabs value={tabValue} onChange={handleTabChange} indicatorColor="secondary" textColor="secondary">
+                    <Tab label="Edit"/>
+                    <Tab label="Preview"/>
+                </Tabs>
+                {tabValue === 0 && (
+                    <TextField
+                        placeholder="Description (you can use markdown)"
+                        color="warning"
+                        multiline
+                        rows={5}
+                        value={description}
+                        onChange={(e) => {
+                            const target = e.target as HTMLInputElement;
+                            setDescription(target?.value || '');
+                        }}
+                    />
+                )}
+                {tabValue === 1 && (
+                    <Box border={1} padding={2} borderRadius={1} borderColor={"#6A6A68"}>
+                        <MuiMarkdown
+                            overrides={{}}>
+                            {description}
+                        </MuiMarkdown>
+                    </Box>
+                )}
+                <FormControl>
+                    <InputLabel id="char-select-label">Character</InputLabel>
+                    <Select
+                        color="warning"
+                        labelId="char-select-label"
+                        id="char-select"
+                        value={selectedCharacter}
+                        label="Character"
+                        onChange={(e) => {
+                            const target = e.target as HTMLInputElement;
+                            const selectedChar = chars.find(char => char.characterId === target.value);
+                            setSelectedCharacter(selectedChar ? selectedChar.characterId : '');
+                        }}
+                    >
+                        {chars.map((char) => (
+                            <MenuItem key={char.characterId} value={char.characterId}>
+                                {char.characterId}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <Divider/>
+                <Stack direction={"row"}>
+                    <EnergyCostGraph cardList={cardList || []}/>
+                    <RarityGraph cardList={cardList || []}/>
+                </Stack>
+                <Typography variant="h5">Cards</Typography>
+                <Typography>Cost: {cardCost}</Typography>
+                <Grid container
+                      spacing={2}>
+                    <Grid xs={3} md={4}>
+                        <Box>
+                            <FixedSizeList
+                                height={700}
+                                width='100%'
+                                itemSize={46}
+                                itemCount={cardList.length}
+                            >
+                                {({index, style}) => (
+                                    <ListItem style={style}>
+                                        <ListItemButton onClick={() => removeCard(cardList[index])}>
+                                            <ListItemText primary={cardList[index].name}/>
+                                        </ListItemButton>
+                                    </ListItem>
+                                )}
+                            </FixedSizeList>
+                        </Box>
+                    </Grid>
+                    <Grid xs={9} md={8}>
+                        <Box marginBottom={2}>
+                            <CustomSearch/>
+                        </Box>
+                        {selectedCharacter &&
+                            <CardsView component={true}
+                                       charClass={chars.find(char => char.characterId === selectedCharacter)?.characterClass || ''}
+                                       secondaryCharClass={chars.find(char => char.characterId === selectedCharacter)?.secondaryCharacterClass || ''}
+                                       onCardClick={addCardToList}/>
+                        }
+                    </Grid>
+                </Grid>
+                <Divider/>
+                <Button variant="contained" onClick={() => uploadDeck(title, description, cardList)}
+                        disabled={isSaving}>Save</Button>
+            </Stack>
+            <Snackbar
+                open={open}
+                sx={{position: 'sticky', marginLeft: 4}}
+                anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
+                autoHideDuration={6000}
+                onClose={handleClose}
             >
-                {chars.map((char) => (
-                    <MenuItem key={char.characterId} value={char.characterId}>
-                        {char.characterId}
-                    </MenuItem>
-                ))}
-            </TextField>
-            <Divider/>
-            <Typography variant="h5">Cards</Typography>
-            <Typography>Cost: {cardCost}</Typography>
-            <Grid container
-                  spacing={2}>
-                <Grid xs={3} md={4}>
-                    <Box style={{maxHeight: 'calc(100vh - 100px)', overflow: 'auto'}}>
-                        <List>
-                            {cardList.map((card, index) => (
-                                <CustomListItem key={index} card={card} removeCard={removeCard}/>
-                            ))}
-                        </List>
-                    </Box>
-                </Grid>
-                <Grid xs={9} md={8}>
-                    <Box marginBottom={2}>
-                        <CustomSearch/>
-                    </Box>
-                    <CardsView component={true}
-                               charClass={chars.find(char => char.characterId === selectedCharacter)?.characterClass || ''}
-                               secondaryCharClass={chars.find(char => char.characterId === selectedCharacter)?.secondaryCharacterClass || ''}
-                               onCardClick={addCardToList}/>
-                </Grid>
-            </Grid>
-            <Divider/>
-            <Button variant="contained" onClick={() => uploadDeck(title, description, cardList)}
-                    disabled={isSaving}>Save</Button>
-        </Stack>
+                <Alert
+                    onClose={handleClose}
+                    severity="success"
+                    variant="filled"
+                >
+                    Deck uploaded.
+                </Alert>
+            </Snackbar>
+            <Snackbar
+                open={openError}
+                sx={{position: 'sticky', marginLeft: 4}}
+                anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
+                autoHideDuration={6000}
+                onClose={handleClose}
+            >
+                <Alert
+                    onClose={handleClose}
+                    severity="error"
+                    variant="filled"
+                >
+                    Unexpected error occurred, please try again later.
+                </Alert>
+            </Snackbar>
+        </div>
     );
 }
