dd54184017e6ff0d043680b18a0697fed06129a2
LociStar <locistar.got@gmail.com>
2024-03-24 12:16:40 +0100

    fixed background and font

--- src/app.tsx
+++ src/app.tsx
@@ -5,9 +5,11 @@ import AppBar from "./components/AppBar.tsx";
 import {oidcConfig} from "./config.tsx";
 import {AuthProvider} from "react-oidc-context";
 import ViewController from "./screens/ViewController.tsx";
-import CantoraOne from "./assets/fonts/font.ttf";
+//import CantoraOne from "./fonts/font.ttf";
 import createAppState from "./utils/AppState.tsx";
 import {createContext} from "preact";
+import backgroundImage from './assets/extended-town.jpg';
+import {useEffect} from "preact/hooks";
 
 declare module '@mui/material/styles' {
     interface Theme {
@@ -50,24 +52,31 @@ const theme = createTheme({
     // },
     palette: {
         mode: 'dark', primary: {
-            main: '#414FD8',
+            main: '#494949',
         }
     },
     typography: {
-        fontFamily: 'CantoraOne',
+        fontFamily: ['Cantora One'].join(','),
     },
     components: {
         MuiCssBaseline: {
             styleOverrides: `
-        @font-face {
-          font-family: 'CantoraOne';
-          font-style: normal;
-          font-display: swap;
-          font-weight: 400;
-          src: local('CantoraOne'), local('CantoraOne-Regular'), url(${CantoraOne}) format('ttf');
-          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-        }
-      `,
+                body::before {
+                  content: "";
+                  position: fixed;
+                  top: 0;
+                  right: 0;
+                  bottom: 0;
+                  left: 0;
+                  background-image: url(${backgroundImage});
+                  background-size: auto 100%; // image will take the full height
+                  background-position-x: var(--mouse-x, center); // use the --mouse-x CSS variable
+                  background-position-y: center;
+                  background-repeat: no-repeat;
+                  filter: blur(0px); // apply blur effect
+                  z-index: -1; // place the pseudo-element behind the content
+                }
+              `,
         },
     },
 });
@@ -75,16 +84,37 @@ const theme = createTheme({
 export const AppState = createContext(createAppState());
 
 export function App() {
+
+    useEffect(() => {
+        const handleMouseMove = (event: MouseEvent) => {
+            if ('ontouchstart' in window || navigator.maxTouchPoints) {
+                return;
+            }
+            const x = event.clientX;
+            const width = window.innerWidth;
+            const percentage = ((x / width) * 4) - 2; // calculate percentage from -10% to 10%
+            const adjustedPercentage = 50 + percentage; // adjust the default position (50%) by the calculated percentage
+            document.body.style.setProperty('--mouse-x', `${adjustedPercentage}%`);
+        };
+
+        window.addEventListener('mousemove', handleMouseMove);
+
+        // Clean up the event listener when the component is unmounted
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+
     return (
         <div className="App">
             <AppState.Provider value={createAppState()}>
-            <AuthProvider {...oidcConfig}>
-            <ThemeProvider theme={theme}>
-                <CssBaseline/>
-                <AppBar/>
-                <ViewController/>
-            </ThemeProvider>
-            </AuthProvider>
+                <AuthProvider {...oidcConfig}>
+                    <ThemeProvider theme={theme}>
+                        <CssBaseline/>
+                        <AppBar/>
+                        <ViewController/>
+                    </ThemeProvider>
+                </AuthProvider>
             </AppState.Provider>
         </div>
     );
