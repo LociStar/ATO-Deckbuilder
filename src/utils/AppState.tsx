29f0872c65fc887a67e5c9b4be6e41b914fb9e30
LociStar <locistar.got@gmail.com>
2024-04-07 15:19:49 +0200

    added and changed states

--- src/utils/AppState.tsx
+++ src/utils/AppState.tsx
@@ -2,6 +2,7 @@ import {signal} from "@preact/signals";
 
 export default function createAppState() {
     const deckId = signal(0);
-    const searchTest = signal('');
-    return { searchText: searchTest, deckId: deckId.value}
+    const searchText = signal('');
+    const appMenuOpen = signal(false);
+    return { searchText: searchText, deckId: deckId.value, appMenuOpen: appMenuOpen}
 }
