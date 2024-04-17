3b337c7d7d6cae4179a5bd2a7bab96d2d377eaa9
LociStar <locistar.got@gmail.com>
2024-04-17 18:12:37 +0200

    change layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,18 @@
-import { Box, Typography, Link } from '@mui/material';
+import { Typography, Link } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 
 export default function Footer() {
     return (
-        <AppBar position="static" color="primary">
+        <AppBar position="static" sx={{alignItems: "center"}} color="primary">
             <Toolbar>
-                <Box sx={{ flexGrow: 1 }} />
+                {/*<Box sx={{ flexGrow: 1 }} />*/}
                 <Typography variant="body1" color="inherit">
+                    <Link color="inherit" href="https://github.com/LociStar/ATO-Deckbuilder">
+                        GitHub
+                    </Link>
+                </Typography>
+                <Typography variant="body1" color="inherit" sx={{ marginLeft: 2 }}>
                     <Link color="inherit" href="/terms-of-service">
                         Terms of Service
                     </Link>
@@ -15,11 +20,6 @@ export default function Footer() {
                 <Typography variant="body1" color="inherit" sx={{ marginLeft: 2 }}>
                     <Link color="inherit" href="/privacy-policy">
                         Privacy Policy
-                    </Link>
-                </Typography>
-                <Typography variant="body1" color="inherit" sx={{ marginLeft: 2 }}>
-                    <Link color="inherit" href="https://github.com/LociStar/ATO-Deckbuilder">
-                        GitHub
                     </Link>
                 </Typography>
             </Toolbar>
