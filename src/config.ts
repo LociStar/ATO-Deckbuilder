4e50ae535b2a32f467558ec5dfd7095b020d0c04
LociStar <locistar.got@gmail.com>
2024-05-22 22:05:13 +0200

    prod setup

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,15 @@
 const AppConfig = {
     REALM_URL: 'https://account.ato-deckbuilder.com/realms/ATO-Deckbuilder/',
     REDIRECT_URL_PROD: 'https://ato-deckbuilder.com/',
-    REDIRECT_URL_DEV: 'http://localhost:5173/',
-    API_URL: 'http://192.168.2.149:8080',
-    //API_URL: 'https://api.ato-deckbuilder.com',
+    //REDIRECT_URL_DEV: 'http://localhost:5173/',
+    //API_URL: 'http://192.168.2.149:8080',
+    API_URL: 'https://api.ato-deckbuilder.com',
 }
 
 const oidcConfig = {
     authority: AppConfig.REALM_URL,
     client_id: "web-app",
-    redirect_uri: AppConfig.REDIRECT_URL_DEV,
+    redirect_uri: AppConfig.REDIRECT_URL_PROD,
     automaticSilentRenew: true,
     onSigninCallback: () => {
         window.history.replaceState(
