16879e212b409754f48b4cbe8c9bedd7d6e4c361
LociStar <locistar.got@gmail.com>
2025-03-03 10:28:48 +0100

    fixed skeleton pre-loading

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -18,12 +18,20 @@ export default function CardComponent({card, onCardClick}: {
     }
 
     return (
-        <div style={{width: '100%', paddingBottom: '151.52%', position: 'relative'}}>
-            {!imageLoaded && <Skeleton variant="rounded" animation="wave" width={CARD_WIDTH} height={CARD_HEIGHT}/>}
+        <div style={{width: CARD_WIDTH, height: CARD_HEIGHT, position: 'relative'}}>
+            {!imageLoaded && (
+                <Skeleton
+                    variant="rounded"
+                    animation="wave"
+                    width={CARD_WIDTH}
+                    height={CARD_HEIGHT}
+                    style={{position: 'absolute', top: 0, left: 0}}
+                />
+            )}
             <img className="zoomEffect"
                  src={AppConfig.API_URL + `/card/image/${card.id}`}
                  alt={`Card ${card.id}`}
-                 style={{position: 'absolute', width: '100%'}}
+                 style={{position: 'absolute', width: '100%', height: '100%', top: 0, left: 0}}
                  onClick={() => onCardClick(card)}
                  onLoad={handleImageLoaded}
             />
